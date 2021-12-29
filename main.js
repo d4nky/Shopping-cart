5fce59225793ae261c677f69c639c528b544403f
Fares Kherbane <kherbanefares@gmail.com>
2021-12-29 07:30:37 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Object list
+const ObjectList = [
+{item: 'Kewkew', price: 0.5},
+{item: 'Ifri', price: 0.5},
+{item: 'Bike', price: 8000},
+{item: 'Console', price: 300},
+{item: 'Steak', price: 45},
+{item: 'Rocket', price: 1},
+{item: 'My Life', price: 999999999999999999999}
+]
+// Assigning vars to html elements
+const itemContainer = document.getElementsByClassName('item-container')
+const addBtn = document.querySelector('.add-btn');
+const removeBtn = document.getElementsByClassName('remove-btn');
+const sum = document.getElementById('total-sum')
+
+
+// Functions 
+addBtn.addEventListener('click', (e) =>{
+    e.preventDefault(); 
+    
+
+})
+
